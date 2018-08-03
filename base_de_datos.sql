a4d2acca09c93e76eeee8c072fd7d2d36f506163
FroylanMtz <32342919+FroylanMtz@users.noreply.github.com>
2018-08-03 01:33:10 -0500

    script de base de datos (correcto)

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -4,51 +4,70 @@ use twitgame;
 
 
 create table usuarios(
-    id int primary key auto_increment not null,
+    id int auto_increment not null,
     nombre varchar(50) not null,
     apellidos varchar(100) not null,
     username varchar(50) not null,
     password varchar(50) not null,
     correo varchar(50) not null,
-    fecha_nacimiento date not null
+    fecha_nacimiento date not null,
+
+    primary key(id)
 );
 
 create table juegos(
-    id int primary key auto_increment not null,
+    id int auto_increment not null,
     nombre varchar(50) not null,
     descripcion varchar(500) not null,
-    genero varchar(50) not null
+    genero varchar(50) not null,
+
+    primary key(id)
+
 );
 
 create table usuarios_juegos(
-    id_usuario int,
-    id_juego int,
+    id_usuario int not null,
+    id_juego int not null,
 
-    constraint "usuarios_juegosFK1" foreign key (id_usuario) references usuarios(id),
-    constraint "usuarios_juegosFK2" foreign key (id_juego) references juegos(id),
+    primary key(id_usuario, id_juego),
+    index(id_usuario),
+    index(id_juego),
+    
+    foreign key (id_usuario) references usuarios(id),
+    foreign key (id_juego) references juegos(id)
 
-    primary key(id_usuario, id_juego);
+    
 );
 
 create table puntajes(
 
-    id int primary key auto_increment not null,
-    id_juego int,
-    id_usuario int,
-    marcador int,
+    id int auto_increment not null,
+    id_juego int not null,
+    id_usuario int not null,
+    marcador int not null,
 
-    constraint "puntajesFK1" foreign key (id_juego) references juegos(id),
-    constraint "puntajesFK2" foreign key (id_usuario) references usuarios(id)
+    primary key(id),
+
+    index(id_juego),
+    index(id_usuario),
+
+    foreign key (id_juego) references juegos(id),
+    foreign key (id_usuario) references usuarios(id)
 
 );
 
 create table seguimiento(
-    id_seguidor int,
-    id_seguido int,
+    id_seguidor int not null,
+    id_seguido int not null,
 
-    constraint "seguimientoFK1" foreign key (id_seguidor) references usuarios(id),
-    constraint "seguimientoFK1" foreign key (id_seguido) references usuarios(id),
+    primary key(id_seguidor, id_seguido),
 
-    primary key(id_seguidor, id_seguido);
+    index(id_seguidor),
+    index(id_seguido),
+
+    foreign key (id_seguidor) references usuarios(id),
+    foreign key (id_seguido) references usuarios(id)
+
+    
 
 );
