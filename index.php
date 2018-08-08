7a4d9a975dbfa9592ecd4df47fac2e4a7de00b41
FroylanMtz <32342919+FroylanMtz@users.noreply.github.com>
2018-08-08 16:43:01 -0500

    12

--- index.php
+++ index.php
@@ -40,8 +40,8 @@
             </nav>
 
             <div class="topnav2">
-                <a class="active" href="#"> <i class="far fa-newspaper"></i> News </a>
-                <a href="#" id="id_juegos_index"> <i class="fas fa-gamepad"></i> Juegos </a>
+                <a href="#"> <i class="far fa-newspaper"></i> News </a>
+                <a class="active" href="#" id="id_juegos_index"> <i class="fas fa-gamepad"></i> Juegos </a>
                 <a href="#"> <i class="fas fa-bell"></i> Notificaciones </a>
             </div>
 
@@ -51,78 +51,30 @@
         
         
         <div class="content_index">
+
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+
+                            <h1> Buscaminas </h1>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+
+                            <h1> Conecta 4 </h1>
+
+                        </div>
+                    </div>
+                </div>
             
-            <div class="row mt-2">
-
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3> Nueva Notificacion </h3>
-                        </div>
-                        <div class="card-body" id="tarjeta_notificacion">
-                            <img src="img/foto_perfil.png" id="foto_notificacion" width="80" height="80">
-                            <p id="p_notificacion"> <h5> Froylan acaba de jugar al juego del gato y ha conseguido 150 puntos </h5> </p>
-                        </div>
-                        </div>
-                </div>
-                
-            </div>
-
-            <div class="row mt-2">
-
-
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3> Otra Notificacion </h3>
-                        </div>
-                        <div class="card-body" id="tarjeta_notificacion">
-                            <img src="img/foto_perfil.png" id="foto_notificacion" width="80" height="80">
-                            <p id="p_notificacion"> <h5> David ha jugado al juego del conecta 4 y ha alcanzado 200 puntos </h5> </p>
-                        </div>
-                        </div>
-                </div>
-
-
-            </div>
-
-
-            <div class="row mt-2">
-
-             
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3> Otra Notificacion </h3>
-                        </div>
-                        <div class="card-body" id="tarjeta_notificacion">
-                            <img src="img/foto_perfil.png" id="foto_notificacion" width="80" height="80">
-                            <p id="p_notificacion"> <h5> Leonardo ha jugado al juego del Buscaminas y ha alcanzado una puntuacion de 50 puntos </h5> </p>
-                        </div>
-                    </div>
-                </div>
-                
-    
-            </div>
-
-            <div class="row mt-2">
-
-             
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3> Otra Notificacion Mas</h3>
-                        </div>
-                        <div class="card-body" id="tarjeta_notificacion">
-                            <img src="img/foto_perfil.png" id="foto_notificacion" width="80" height="80">
-                            <p id="p_notificacion"> <h5> Alarcon ha jugado al juego del Tic Tac Toe y ha alcanzado una puntuacion de 100 puntos </h5> </p>
-                        </div>
-                    </div>
-                </div>
-                
-    
-            </div>
-
+            <div>
         </div>
 
 
